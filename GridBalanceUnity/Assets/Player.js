f720ca82719fbd59a9d487e07bbf32ee95feaee3
Kevin Beirne <kmbeirne@gmail.com>
2013-02-04 14:03:47 +0000

    Fixed file loading/saving, added timer and reservoir level

--- GridBalanceUnity/Assets/Player.js
+++ GridBalanceUnity/Assets/Player.js
@@ -7,66 +7,84 @@ private var moveDirection : Vector3 = Vector3.zero;
 
 var BASE_TURBINE_VALUE = 20;
 var BASE_PUMP_VALUE = 20;
-var BASE_RESERVOIR_VALUE = 100;
+var BASE_RESERVOIR_VALUE = 100.0;
 var speed = 30;
-var maxSpeed = 50;
+var maxSpeed = 60;
 
 private var waterLeft;
 private var money;
+private var WATER_LOSS_PER_SECOND = 5.0;
+private var WATER_REGAIN_RATIO = 0.95;
+private var timeAlive = 0.0;
 
 function Start () 
 {
-Debug.Log("START");
-	//Load upgrade info
-	PlayerInfoHandler.LoadPlayerInfo();
-Debug.Log("save");
-	PlayerInfoHandler.SavePlayerInfo();
-Debug.Log("done");
-	//waterLeft = BASE_RESERVOIR_VALUE * reservoirModifier;
+	waterLeft = BASE_RESERVOIR_VALUE;
+	Debug.Log("START");
+		//Load upgrade info
+		PlayerInfoHandler.LoadPlayerInfo();
+	Debug.Log("save");
+		PlayerInfoHandler.SavePlayerInfo();
+	Debug.Log("done");
+		//waterLeft = BASE_RESERVOIR_VALUE * reservoirModifier;
+}
+
+function Update() 
+{
+	var controller : CharacterController = GetComponent(CharacterController);
+	moveDirection.x = speed;	
+	timeAlive += Time.deltaTime;
+	
+    if (Physics.Raycast(transform.position, -transform.up, 8)) {
+        var name = Application.loadedLevelName;
+        SwitchScene.SwitchToLevel(name);
+    }
+
+    if(Input.GetKey("a")){
+    	speed = 60;
+	}else if(Input.GetKey("s")){
+    	speed = 15;
+	}else{
+		speed = 30;
+	}
+		if(Input.GetKey("space")){
+		waterLeft -= WATER_LOSS_PER_SECOND * Time.deltaTime;
+		MoveUp();
+    }
+	else{
+		waterLeft += WATER_LOSS_PER_SECOND * Time.deltaTime * WATER_REGAIN_RATIO;
+		if(waterLeft > BASE_RESERVOIR_VALUE) waterLeft = BASE_RESERVOIR_VALUE;
+		MoveDown();
+    }
+	
+    controller.Move(moveDirection * Time.deltaTime);
+}
+
+function OnGUI()
+{
+	GUI.Label(Rect(150, 10, 200, 50), "Water Left: " + parseInt(waterLeft));
+	//var twoPlacedFloat = parseFloat(timeAlive).toFixed(2);
+	GUI.Label(Rect(50, 10, 200, 50), "Time: " + timeAlive.ToString("F2"));
 }
 
 function MoveUp()
 {
 	//var move = BASE_TURBINE_VALUE * turbineModifier * Time.deltaTime;
-	moveDirection.z += BASE_TURBINE_VALUE * Time.deltaTime; // insert turbineModifier when i'ts implemented
+	moveDirection.z += BASE_TURBINE_VALUE * Time.deltaTime; // insert turbineModifier when it's implemented
 	if(moveDirection.z >= maxSpeed)
 	{
 		moveDirection.z = maxSpeed;
 	}
+	
 }
 
 function MoveDown()
 {
 	//var move = BASE_PUMP_VALUE * pumpModifier * Time.deltaTime;
 	moveDirection.z -= BASE_PUMP_VALUE * Time.deltaTime;
-}
-
-function Update() 
-{
-	var controller : CharacterController = GetComponent(CharacterController);
-	moveDirection.x = speed;
-    if (Physics.Raycast(transform.position, -transform.up, 8)) {
-        var name = Application.loadedLevelName;
-        SwitchScene.SwitchToLevel(name);
-    }
-
-	if(Input.GetKey("space")){
-		MoveUp();
-    }
-	else{
-		MoveDown();
-    }
-
-    if(Input.GetKey("a")){
-    	speed = 60;
-	}
-    else if(Input.GetKey("s")){
-    	speed = 15;
-	}
-    else{
-		speed = 30;
-	}
 	
-    controller.Move(moveDirection * Time.deltaTime);
+	if(moveDirection.z <= -maxSpeed)
+	{
+		moveDirection.z = -maxSpeed;
+	}
 }
-
--- GridBalanceUnity/Assets/PlayerInfoHandler.js
+++ GridBalanceUnity/Assets/PlayerInfoHandler.js
@@ -6,7 +6,7 @@ class PlayerInfoHandler
 	static var reservoirLevel;
 	
 	private static var infoFile = "PlayerInfo.gbu";
-	private static var infoFile2 = "PlayerInfo.gbu";
+	//private static var infoFile2 = "PlayerInfo.gbu";
 	/*
 	 * Loads player information from a text file
 	 */
@@ -19,16 +19,11 @@ class PlayerInfoHandler
 	
 		var lastLevel = null;
 	    var lines = fileContents.Split("\n"[0]);
-	    
-
+    	
     	money = lines[parseInt(0)].Split(" "[0])[1];
     	turbineLevel = lines[parseInt(1)].Split(" "[0])[1];
     	pumpLevel = lines[parseInt(2)].Split(" "[0])[1];
     	reservoirLevel = lines[parseInt(3)].Split(" "[0])[1];
-    	Debug.Log("Monay!: " + money);
-    	Debug.Log("turbineModifier: " + turbineLevel);
-    	Debug.Log("pumpModifier: " + pumpLevel);
-    	Debug.Log("reservoirModifier: " + reservoirLevel);
 	}
 	
 	/*
@@ -37,18 +32,12 @@ class PlayerInfoHandler
 	static function SavePlayerInfo()
 	{
 		var NUM_PARAMS = 4;
-	    var sw = new StreamWriter(Application.dataPath + "/" + infoFile2);
+	    var sw = new StreamWriter(Application.dataPath + "/" + infoFile);
 	    
-    	Debug.Log("Monay!: " + money);
-    	Debug.Log("turbineModifier: " + turbineLevel);
-    	Debug.Log("pumpModifier: " + pumpLevel);
-    	Debug.Log("reservoirModifier: " + reservoirLevel);
-    	
-	    sw.Write("Money " + money);
-	    sw.Write("TurbineLevel " + turbineLevel);
-	    sw.Write("PumpLevel " + pumpLevel);
-	    sw.Write("ReservoirLevel " + reservoirLevel);
-	    
+	    sw.Write("Money " + "100000"
+	    	+ "\nTurbineLevel " + "1"
+	    	+ "\nPumpLevel " + "2"
+	    	+ "\nReservoirLevel " + "3");
 	    sw.Close();
 	}
 }

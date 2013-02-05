50c394e72ae7887b2f019c494dcbf31cdd96a0b5
Senchy <senchy5605@hotmail.com>
2013-02-05 11:21:07 +0000

    temp commit to get kevins code
    p.s. might break the menu tempoaryly

--- GridBalanceUnity/Assets/UpgradeMenu.js
+++ GridBalanceUnity/Assets/UpgradeMenu.js
@@ -1,3 +1,5 @@
+//#pragma strict
+	
 	private var ScreenWidth = Screen.width;
 	private var ScreenHeight = Screen.height;
 	private var Width = ScreenWidth / 4;
@@ -8,12 +10,13 @@
 	private var SwitchMessageBox = false;
 	private var messageBoxWidth = 160;
 	private var messageBoxHeight = 40;
-	var EducationalText = "EducationalText";
-	var NameOfChange = "NameOfChange";
-	var ChangeValue = "ChangeValue";
-	var Cost = 120;
-	var message = "";
+	private var educationalText : String = "Educational Text";
+	private var nameOfChange : String = "Name Of Change";
+	private var changeValue : String = "Change Value";
+	private var cost = 120;
+	private var message : String = "";
 function Start () {
+	PlayerInfoHandler.LoadPlayerInfo();
 }
 
 function Update () {
@@ -32,6 +35,7 @@ function OnGUI () {
     if(SwitchGUI){
 		GUI.Box (new Rect(ScreenWidth - Width,0,Width,ScreenHeight), "");
 		//leftSide
+			// Upgrade Pump
 		if(GUI.Button (new Rect ((ScreenWidth - Width) + (ButtonWidth / 3),
 		ButtonHeight / 5, 
 		ButtonWidth, ButtonHeight), "")) {
@@ -40,59 +44,76 @@ function OnGUI () {
 		if(GUI.Button (new Rect ((ScreenWidth - Width) + (ButtonWidth / 3),
 		ButtonHeight + 2 * (ButtonHeight / 5), 
 		ButtonWidth, ButtonHeight), "")) {
+			
 			SwitchGUI = false;
 		}
 		if(GUI.Button (new Rect ((ScreenWidth - Width) + (ButtonWidth / 3),
 		2 * ButtonHeight + 3 * (ButtonHeight / 5), 
 		ButtonWidth, ButtonHeight), "")) {
+			
 			SwitchGUI = false;
 		}
 		//rightside
+				//Turbine
 		if(GUI.Button (new Rect ((ScreenWidth - Width) + ButtonWidth + (2 *(ButtonWidth / 3)),
 		ButtonHeight / 5, 
 		ButtonWidth, ButtonHeight), "")) {
+			
 			SwitchGUI = false;
 		}
+				//Expand Resivour
 		if(GUI.Button (new Rect ((ScreenWidth - Width) + ButtonWidth + (2 *(ButtonWidth / 3)),
 		ButtonHeight + 2 * (ButtonHeight / 5), 
 		ButtonWidth, ButtonHeight), "")) {
+			
 			SwitchGUI = false;
 		}
+				//
 		if(GUI.Button (new Rect ((ScreenWidth - Width) + ButtonWidth + (2 * (ButtonWidth / 3)),
 		2 * ButtonHeight + 3 * (ButtonHeight / 5), 
 		ButtonWidth, ButtonHeight), "")) {
+			
 			SwitchGUI = false;
 		}
 		//return
 		if(GUI.Button (new Rect ((ScreenWidth - Width) + ButtonWidth ,
 		3 * ButtonHeight + 7 * (ButtonHeight / 5) ,
 		ButtonWidth, ButtonHeight/2), "Return")) {
+			PlayerInfoHandler.SavePlayerInfo();
 			SwitchScene.SwitchToMainMenu();
 		}
 	}
 	else
 	{
+		////Display Message
 		if(SwitchMessageBox){
 			GUI.Box (new Rect((ScreenWidth/2)- (messageBoxWidth/2),
 					(ScreenHeight/2) - (messageBoxHeight/2),
 					messageBoxWidth,messageBoxHeight), message);
 		}
-		if(GUI.Button (new Rect ((ScreenWidth - Width) - 10,
+		////Information Labels
+		GUI.Label(new Rect((ScreenWidth - Width) - 80,30,Width + 50, 165), educationalText);
+		GUI.Label(new Rect((ScreenWidth - Width) - 80,3 *(ScreenHeight/ 5),Width + 50, 30), nameOfChange + " :");
+		GUI.Label(new Rect(ScreenWidth - ((2*Width) / 3),3 *(ScreenHeight/5),Width, 30), changeValue);
+		GUI.Label(new Rect((ScreenWidth - Width) - 80,3 * ButtonHeight + 7 * (ButtonHeight / 5) - 30,Width + 50, 30), "Price :  " + cost);
+		GUI.Label(new Rect(20,30,200, 30), "$ :  ");// + PlayerInfoHandler.money);
+		///BUTTONS
+		if(GUI.Button (new Rect ((ScreenWidth - Width) - 70,
 		3 * ButtonHeight + 7 * (ButtonHeight / 5) ,
-		ButtonWidth, ButtonHeight/2), "Buy")) {
+		ButtonWidth + 10, ButtonHeight/2), "Buy")) {
 			SwitchMessageBox = true;
-			if(PlayerInfoHandler.money < Cost){
+			if(PlayerInfoHandler.money < cost){
 				message = "Not Enough Money";
 			}
 			else
 			{
-				PlayerInfoHandler.money -= Cost;
+				PlayerInfoHandler.money -= cost;
 				message = "Purchased";
 			}
 		}
-		if(GUI.Button (new Rect ((ScreenWidth - Width) + 2 * ButtonWidth - 10 ,
+		if(GUI.Button (new Rect ((ScreenWidth - Width) + 2 * ButtonWidth - 40 ,
 		3 * ButtonHeight + 7 * (ButtonHeight / 5) ,
-		ButtonWidth, ButtonHeight/2), "Cancel")) {
+		ButtonWidth + 10, ButtonHeight/2), "Cancel")) {
 			SwitchGUI = true;
 			SwitchMessageBox = false;
 		}
